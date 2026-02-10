5c5d75de22d6997dd7030ef7518618cdd7018865
Joshua Reece <joshua.t.reece.mil@army.mil>
2026-02-10 14:56:49 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your choice: ");
+        //int choice = scanner.nextInt();
+
+        String choice = scanner.nextLine();
+
+        switch (choice) {
+            case "A":
+                System.out.println("hello");
+                break;
+            case "B":
+                System.out.println("world");
+                break;
+            case "D":
+                System.out.println("abc");
+                break;
+            default:
+                System.out.println("invalid choice");
+        }
+
+        System.out.print("after the switch");
+
+    }
+}
